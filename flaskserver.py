be107058b25026611ed40cdfd58b6f9e4c775674
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2025-05-20 15:49:56 +0100

    skeleton functions in DAO, flask server functions updates

--- flaskserver.py
+++ flaskserver.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
+from scidataDAO import sciDAO
 
 app = Flask(__name__)
 
@@ -7,39 +8,55 @@ def index():
         return "This is index"
 
 # get all collections
-@app.route('/data', methods=['GET'])
-def getdata():
+@app.route('/datacollections', methods=['GET'])
+def getcollections():
         # get info on all collections
+        return jsonify(sciDAO.getAllCollections())
         
-        return "getdata"
-
 # get all collection types
-@app.route('/data', methods=['GET'])
+@app.route('/datacollectiontypes', methods=['GET'])
 def getcollectiontypes():
         # get info on all collection types
-        
-        return "getdata"
+        return jsonify(sciDAO.getAllCollectionTypes())
 
 # get data by id
-@app.route('/data/<int:id>', methods=['GET'])
+@app.route('/datacollections/<int:id>', methods=['GET'])
 def getdatabycid(id):
         # get data by collection id
-        return "get data by id"
+        return jsonify(sciDAO.getDataByID(id))
 
-# create
-@app.route('/data', methods=['POST'])
+# create collection
+@app.route('/datacollections', methods=['POST'])
+def createcollection():
+        # create new collection of data
+        jsonstring = request.json
+        collection = {}
+        if "name" not in jsonstring:
+                abort(401)
+        collection["name"] = jsonstring["name"]
+        collection["collectiontypeid"] = jsonstring["collectiontypeid"]
+        collection["startdate"] = jsonstring["startdate"]
+        collection["enddate"] = jsonstring["enddate"]
+        collection["locationname"] = jsonstring["locationname"]
+        collection["measurement"] = jsonstring["measurement"]
+        collection["units"] = jsonstring["units"]
+        return jsonify(sciDAO.createCollection)
+
+# create data
+@app.route('/datacollections', methods=['POST'])
 def createdata():
+        # create new data entry
         jsonstring = request.json
         return f"create {jsonstring}"
 
 # update
-@app.route('/data/<int:id>', methods=['PUT'])
+@app.route('/datacollections/<int:id>', methods=['PUT'])
 def updatedata(id):
         jsonstring = request.json
         return f"update {id} {jsonstring}"
 
 # delete
-@app.route('/data/<int:id>', methods=['DELETE'])
+@app.route('/datacollections/<int:id>', methods=['DELETE'])
 def deletedata(id):
         return f"delete {id}"
 
--- scidataDAO.py
+++ scidataDAO.py
@@ -6,36 +6,60 @@ class SciDAO:
     # get all collections          
     def getAllCollections(self):
         #TODO implement
-        return [{"id":"1", "collectiontypeid":1, "startdate":"2025-04-04 00:00:00", "enddate":"2025-06-04 00:00:00", "locationname":"Rockall", "measurement":"Temperature", "units":"Celsius"}]
+        return {"name":1, "collectiontypeid":1, "startdate":"2025-04-04 00:00:00", "enddate":"2025-06-04 00:00:00", "locationname":"Rockall", "measurement":"Temperature", "units":"Celsius"}
     # get all collection types 
     def getAllCollectionTypes(self):
         #TODO implement
-        return [{"id":"1", "collectiontype":"time series"}]
+        return {"id":1, "collectiontype":"time series"}
     # find by id
     def getDataByID(self, id):
         #TODO implement
-        return [{"id":"1", "collectionid":1, "datum":"2.2", "lat":"0", "long":"0", "datetime":"2025-06-04 00:00:00"}]
-    # create a book
-    def create(self, book):
-        return {"id":1,"title":"blah","author":"someone","price":999}
-    #update 
-    def update(self,id , book):
-        return {"id":1,"title":"blah","author":"someone","price":999}
-    # delete a book of a given id    
-    def delete(self, id):
+        return {"id":1, "collectionname":"collection1", "datum":"2.2", "lat":"0", "long":"0", "datetime":"2025-06-04 00:00:00"}
+    # create a collection
+    def createCollection(self, colname):
+        #TODO implement
+        return {"name":"collection1", "collectiontypeid":1, "startdate":"2025-04-04 00:00:00", "enddate":"2025-06-04 00:00:00", "locationname":"Rockall", "measurement":"Temperature", "units":"Celsius"}
+    # add data to collection
+    def createData(self, colname, data):
+        #TODO implement
+        return {"id":1, "collectionname":"collection1", "datum":"2.2", "lat":"0", "long":"0", "datetime":"2025-06-04 00:00:00"}
+    # update collection
+    def updateCollection(self, colname):
+        #TODO implement
+        return {"name":"collection1", "collectiontypeid":1, "startdate":"2025-04-04 00:00:00", "enddate":"2025-06-04 00:00:00", "locationname":"Rockall", "measurement":"Temperature", "units":"Celsius"}
+    # update data
+    def updateData(self, colname, data):
+        #TODO implement
+        return {"id":1, "collectionname":"collection1", "datum":"2.2", "lat":"0", "long":"0", "datetime":"2025-06-04 00:00:00"}
+    # delete a collection    
+    def deleteCollection(self, id):
+        #TODO implement
+        return True
+    # delete data   
+    def deleteData(self, id):
+        #TODO implement
         return True
         
 sciDAO = SciDAO()
 
 if __name__ == "__main__":
-    book = {"id":1,"title":"blah","author":"someone","price":999} 
-    print ("test getall")
-    print (f"\t{sciDAO.getAll()}")
-    print ("test findById(1)")
-    print (f"\t{sciDAO.findByID(1)}")
-    print ("test create")
-    print (f"\t{sciDAO.create(book)}")
-    print ("test update")
-    print (f"\t{sciDAO.update(1,book)}")
-    print ("test delete")
-    print (f"\t{sciDAO.delete(1)}")
+    collection = {"name":"collection1", "collectiontypeid":1, "startdate":"2025-04-04 00:00:00", "enddate":"2025-06-04 00:00:00", "locationname":"Rockall", "measurement":"Temperature", "units":"Celsius"} 
+    data = {"id":1, "collectionname":"collection1", "datum":"2.2", "lat":"0", "long":"0", "datetime":"2025-06-04 00:00:00"}
+    print ("test getallCollections")
+    print (f"\t{sciDAO.getAllCollections()}")
+    print ("test getallCollectionTypes")
+    print (f"\t{sciDAO.getAllCollectionTypes()}")
+    print ("test finddataById(1)")
+    print (f"\t{sciDAO.getDataByID(1)}")
+    print ("test create collection")
+    print (f"\t{sciDAO.createCollection(collection)}")
+    print ("test create data")
+    print (f"\t{sciDAO.createCollection(data)}")
+    print ("test update collection")
+    print (f"\t{sciDAO.updateCollection(collection)}")
+    print ("test update data")
+    print (f"\t{sciDAO.updateData(collection, data)}")
+    print ("test delete collection")
+    print (f"\t{sciDAO.deleteCollection(1)}")
+    print ("test delete data")
+    print (f"\t{sciDAO.deleteData(1)}")
